c5d99a3df74c09071c15202bf666e540cad58e5a
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-05-16 15:48:25 +0100

    progress with javascript

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//object to track the values
+//OBJECT TO TRACK VALUES
 const calculator = {
   displayValue: "0", // our value
   firstOperand: null, //a first operand of expression
@@ -6,7 +6,7 @@ const calculator = {
   operator: null, // the operator of expression
 };
 
-// update display function
+// UPDATE DISPLAY FUNCTION
 function updateDisplay() {
   // select an element with class of "space"
   const display = document.querySelector(".screen");
@@ -16,7 +16,7 @@ function updateDisplay() {
 
 updateDisplay();
 
-//key presses
+//BUTTONS PRESSES
 const buttons = document.querySelector(".buttons");
 
 buttons.addEventListener("click", (event) => {
@@ -31,13 +31,15 @@ buttons.addEventListener("click", (event) => {
 
   //if element buttons contains class operator
   if (target.classList.contains("operator")) {
-    console.log("operator", target.value);
+    workWithOperator(target.value);
+    updateDisplay();
     return;
   }
 
   //if element buttons contains class decimal
   if (target.classList.contains("dot")) {
-    console.log("decimal", target.value);
+    inputDot(target.value);
+    updateDisplay();
     return;
   }
 
@@ -47,6 +49,56 @@ buttons.addEventListener("click", (event) => {
     return;
   }
 
-  //if element buttons does not contain any of above, it means numbe was clicked
-  console.log("digit", target.value);
+  //if element buttons does not contain any of above, it means number was clicked and we use inputNumber function to display a number
+  inputNumber(target.value);
+  updateDisplay();
 });
+
+//INPUT THE NUMBERS
+function inputNumber(number) {
+  const displayValue = calculator.displayValue;
+  const waitingForSecondOperand = calculator.waitingForSecondOperand;
+  // to not overwrite firstvalue
+  if (waitingForSecondOperand === true) {
+    calculator.displayValue = number;
+    calculator.waitingForSecondOperand = false;
+  } else {
+    // if value on the screen is 0, we replace it with a number
+    if (displayValue === "0") {
+      calculator.displayValue = number;
+      // if it is not zero we add a number following a number on the screen
+    } else {
+      calculator.displayValue = displayValue + number;
+    }
+  }
+  console.log(calculator);
+}
+
+// ADDING DOT IF IT IS NOT THERE AT THE MOMENT WHEN WE PRESS IT
+function inputDot(dot) {
+  if (!calculator.displayValue.includes(dot)) {
+    calculator.displayValue += dot;
+  }
+}
+
+// FUNCTION TO WORK WITH OPERATORS
+function workWithOperator(nextOperator) {
+  // destructuring the calculator object
+  const { firstOperand, displayValue, operator } = calculator;
+  //could write instead of this in more specific way
+  // const firstOperand = calculator.firstOperand;
+  // const displayValue = calculator.displayValue;
+  // const operator = calculator.operator;
+  const inputValue = parseFloat(displayValue);
+  // we replace a string with float number
+  if (firstOperand === null && !isNaN(inputValue)) {
+    //we are checking if first operand is equal null as we defined in the beginning and if inputValue is a number
+    calculator.firstOperand = inputValue;
+    //now first operand of our calculator is equal to the value we wrote
+  }
+
+  calculator.waitingForSecondOperand = true;
+  // we change calculator key waitingForSecondOperator to true, because first operand has been entered
+  calculator.operator = nextOperator;
+  console.log(calculator);
+}
